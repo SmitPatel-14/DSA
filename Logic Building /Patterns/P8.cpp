b812fa446d3bc49a201fee759ff7104cf0eb34d9
SmitPatel-14 <sp3896508@gmail.com>
2025-07-04 17:43:55 +0000

    new Patterns

---
+++ Logic Building /Patterns/P8.cpp
@@ -0,0 +1,29 @@
+// *********
+//  ******* 
+//   *****  
+//    ***   
+//     *  
+#include<iostream>
+using namespace std;
+
+int main(){
+      int n;
+    cout<<"Ernter thye number";
+    cin>>n;
+     for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
