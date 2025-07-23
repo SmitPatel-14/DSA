a638b23cadab2b0a8af5ab385cf7e4d05e957990
SmitPatel-14 <sp3896508@gmail.com>
2025-07-23 03:48:02 +0000

    insertion sort

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -33,3 +33,16 @@ void bubblesort(int arr[], int n) {
         }
     }
 }
+
+//Insertion Sort
+void Insertionsort(int arr[,int n]){
+    for(int i=0;i<n;i++){
+        int j =i;
+        while(j>0 &&arr[j-1]>arr[j]){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+    }
+}
