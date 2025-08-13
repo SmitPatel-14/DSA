2e84e5e8cf2f1e0ca0fc6d16d49c1d0862313208
SmitPatel-14 <sp3896508@gmail.com>
2025-08-13 17:53:32 +0000

    .

--- Array/moreaary.cpp
+++ Array/moreaary.cpp
@@ -25,3 +25,31 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+        
+        // Step 1: Find the first index 'i' from the end where nums[i] < nums[i+1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            // Step 2: Find index 'j' from the end where nums[j] > nums[i]
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap nums[i] and nums[j]
+            swap(nums[i], nums[j]);
+        }
+        
+        // Step 4: Reverse the suffix starting from i+1
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
+
