49726fea5df05d88dae9601a9fb737b1e2758aef
SmitPatel-14 <sp3896508@gmail.com>
2025-08-21 18:02:47 +0000

    soetll

---
+++ Link List/sortll.cpp
@@ -0,0 +1,20 @@
+
+//brute
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+       vector<int> arr;
+       ListNode* temp = head;
+       while(temp!=nullptr){
+        arr.push_back(temp->val);
+        temp = temp->next;
+        }
+        sort(arr.begin(), arr.end());
+        temp = head;
+        for(int i=0;i<arr.size();i++){
+            temp->val = arr[i];
+            temp = temp->next;
+        } 
+        return head;
+    }
+};
